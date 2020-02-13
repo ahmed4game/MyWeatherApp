bd19e56f416e2865f8d3aaa62d11c44fc343a0da
Ahmed <ahmedmeeran.thamby@tessolve.com>
2020-02-13 19:01:52 +0530

    Initial Code commit with few Helper Classes.

---
+++ app/src/main/java/com/ahmed/myapplication/Constant.java
@@ -0,0 +1,5 @@
+package com.ahmed.myapplication;
+
+public class Constant {
+    public static String BASE_URL = "";
+}
---
+++ app/src/main/java/com/ahmed/myapplication/MyApplication.java
@@ -0,0 +1,12 @@
+package com.ahmed.myapplication;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
